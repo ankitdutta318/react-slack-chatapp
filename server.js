ddbcfd9c0e06326b6358e97ec34bf4bf1bd65eed
cruxbreaker <geekankit318@gmail.com>
2018-08-18 12:55:28 +0530

    Created Chatkit instance and created users on server

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:9f3457c9-294a-46e0-9993-903195ceca93',
+  key: 'ebab043a-afa8-4f66-8995-1375e589c8cb:y2gFvG8GyDnaJkO5Hnj3a2Dc0evtOn8h7f4SAgd+z8Q=',
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({ 
+  id: username, 
+  name: username 
+      })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
