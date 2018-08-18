481bacdbf3bce22588ce462a83b8b219f7ab8c44
cruxbreaker <geekankit318@gmail.com>
2018-08-18 13:41:44 +0530

    Connecting to Chatkit instance

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+import Chatkit from '@pusher/chatkit'
 
 class ChatScreen extends Component {  
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: {}
+    }
+  }
+  
+  componentDidMount () {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'v1:us1:9f3457c9-294a-46e0-9993-903195ceca93',
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: 'http://localhost:3001/authenticate',
+      }),
+    })
+
+  chatManager
+    .connect()
+    .then(currentUser => {
+      console.log('Current User: ', currentUser)
+      this.setState({ currentUser })
+    })
+    .catch(error => console.error('error', error))
+}
+    
+
   render() {
     return (
       <div>
